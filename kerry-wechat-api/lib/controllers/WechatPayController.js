753aca13f1b4fbde48b6e3bd7ecfbcf6279ddcb7
cuiyang <cuiyang>
2016-07-29 17:18:22 +0800

    no message

--- kerry-wechat-api/lib/controllers/WechatPayController.js
+++ kerry-wechat-api/lib/controllers/WechatPayController.js
@@ -14,8 +14,8 @@ var _ = require("lodash"),
      partnerKey: "w4go19um14n73r2v2v3wvderavvscgz0",  //w4go19um14n73r2v2v3wvderavvscgz0  API密钥，嘉里不夜城
      appId: "wx59b13639314be7c8",
      mchId: "1352525102",
-     notifyUrl: "http://www.weixin.qq.com/wxpay/pay.php",
-     pfx: fs.readFileSync("../../cert/apiclient_cert.p12")
+     notifyUrl: "http://www.weixin.qq.com/wxpay/pay.php"//,
+     //pfx: fs.readFileSync("../../cert/apiclient_cert.p12")
    };
    var payment = new Payment(initConfig);
 
@@ -32,10 +32,11 @@ router.post("/WechatPay", function(req, res, next) {
   attach: '{"部位":"三角"}',     //附加数据
   out_trade_no: '20160628080001',   //订单号
   total_fee: 10 * 100,     //总金额
-  spbill_create_ip: req.ip,  //下单IP
-  openid: 'wechat_ossPrw6Uu6gK69mwwyv151LbPgJE',
+  spbill_create_ip: "10.0.0.35",  //下单IP
+  openid: 'oc4kVwVHYTTQhWq7hrc_rgMBSpjI',
   trade_type: 'JSAPI'
 };
+console.log("IP  "+ req.ip);
 
 payment.getBrandWCPayRequestParams(order, function(err, payargs){
 console.log(err);
