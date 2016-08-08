58616f710c5f7501e9b8c94444493232308a3791
harry wang <harrywang624@gmail.com>
2016-08-08 18:57:31 +0800

    update menu.js

--- kerry-wechat-api/lib/menu.js
+++ kerry-wechat-api/lib/menu.js
@@ -7,8 +7,68 @@ module.exports = [
     "link": "/bill"
   },
   {
+    "name": "自定义菜单",
+    "icon": "ti-view-grid",
+    "link": "/menusetting"
+  },
+  {
+    "name": "自动回复",
+    "icon": "ti-gallery",
+    "link": "/messagesetting"
+  },
+  {
+    "name": "图文消息",
+    "icon": "ti-image",
+    "link": "/newspush"
+  },
+  {
+    "name": "户号管理",
+    "icon": "ti-home",
+    "link": "/unit"
+  },
+  {
+    "name": "业主管理",
+    "icon": "ti-id-badge",
+    "link": "/user"
+  },
+  {
+    "name": "微信用户管理",
+    "icon": "ti-comments",
+    "link": "/wechatUser"
+  },
+  {
+    "name": "消息管理",
+    "icon": "ti-email",
+    "link": "/pushMessage"
+  },
+  {
+    "name": "公告与活动",
+    "icon": "ti-announcement",
+    "link": "/billboard"
+  },
+  {
+    "name": "意见反馈",
+    "icon": "ti-comment-alt",
+    "link": "/suggestions"
+  },
+  {
+    "name": "意见反馈",
+    "icon": "ti-comment-alt",
+    "link": "/suggestions"
+  },
+  {
     "name": "个人中心",
     "icon": "ti-user",
     "link": "/usercenter"
-  }
+  },
+  {
+    "name": "系统设置",
+    "icon": "ti-settings",
+    "link": "/syssetting"
+  },
+  {
+    "name": "系统账号管理",
+    "icon": "ti-panel",
+    "link": "/backgroundUsers"
+  },
 ]
