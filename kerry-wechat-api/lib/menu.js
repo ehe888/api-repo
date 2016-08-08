b922c749e5172e9e03af0c13ea482e0bff28e52d
jiajun.wang <jiajun.wang@aivics.com>
2016-08-08 18:28:25 +0800

    add menu list

---
+++ kerry-wechat-api/lib/menu.js
@@ -0,0 +1,14 @@
+"use strict"
+
+module.exports = [
+  {
+    "name": "账单管理",
+    "icon": "ti-files",
+    "link": "/bill"
+  },
+  {
+    "name": "个人中心",
+    "icon": "ti-user",
+    "link": "/usercenter"
+  }
+]
