0988433848163296f6bf5f1f67aba112b635d3bd
jiajun.wang <jiajun.wang@aivics.com>
2016-08-15 15:54:24 +0800

    change query bill api

--- kerry-wechat-api/lib/controllers/BillManagerController.js
+++ kerry-wechat-api/lib/controllers/BillManagerController.js
@@ -21,8 +21,93 @@ module.exports = function(app, db, options){
         appId = param.appId,
         offset = param.offset || 0,
         limit = param.limit || 20,
+        is_pay = param.is_pay,
         unit_desc = param.unit_desc || ""
 
+    queryBills(start_time, end_time, appId, offset, limit, unit_desc, is_pay,
+      function(error, results, offset, limit, count) {
+
+        var data = [];
+        for (var i = 0; i < results.length; i++) {
+          var row = results[i];
+          var bill = _.find(data, function(o) {
+            return row.id == o.id
+          })
+          if (!bill) {
+            data.push({
+              id: row.id,
+              bill_number: row.bill_number,
+              year: row.year,
+              month: row.month,
+              print_data: row.print_data,
+              is_push: row.is_push,
+              username: row.username,
+              active: row.active,
+              created_at: row.created_at,
+              updated_at: row.updated_at,
+              deleted_at: row.deleted_at,
+              unit_id: row.unit_id,
+              property_bill_lines: [{
+                id: row.bill_line_id,
+                work_order_number: row.work_order_number,
+                description: row.description,
+                taxable_amount: row.taxable_amount,
+                tax: row.tax,
+                gross_amount: row.gross_amount,
+                is_pay: row.is_pay,
+                expire_date: row.expire_date
+              }],
+              unit: [{
+                id: row.unit_id,
+                unit_number: row.unit_number,
+                unit_desc: row.unit_desc
+              }]
+            })
+          }
+          else {
+            bill.property_bill_lines.push({
+              id: row.bill_line_id,
+              work_order_number: row.work_order_number,
+              description: row.description,
+              taxable_amount: row.taxable_amount,
+              tax: row.tax,
+              gross_amount: row.gross_amount,
+              is_pay: row.is_pay,
+              expire_date: row.expire_date
+            })
+
+            bill.unit.push({
+              id: row.unit_id,
+              unit_number: row.unit_number,
+              unit_desc: row.unit_desc
+            })
+          }
+        }
+
+        if (error) {
+          return res.status(500).json({
+            success: false,
+            error: error,
+            errMsg: error.message
+          })
+        }
+        return res.json({
+          success: true,
+          data: data,
+          offset: offset,
+          limit: limit,
+          count: count
+        })
+
+      })
+
+  })
+
+  router.post("export", function(req, res, next) {
+
+  })
+
+  function queryBills(start_time, end_time, appId, offset, limit, unit_desc, is_pay, callback) {
     var billOption = ""
     if (unit_desc && unit_desc.length > 0) {
       billOption += "'%"+unit_desc+"%'"
@@ -63,70 +148,14 @@ module.exports = function(app, db, options){
     if (timeOption.length > 0) {
       query += ' AND ' + timeOption
     }
-    if (typeof param.is_pay != 'undefined' && param.is_pay != null) {
-      query += ' AND is_pay=' + param.is_pay
+    if (typeof is_pay != 'undefined' && is_pay != null) {
+      query += ' AND is_pay=' + is_pay
     }
 
     query += ' ORDER BY id DESC offset ? limit ? ) x ON vw_property_bill.id = x.id ORDER BY bill_line_id DESC;'
 
     sequelize.query(query, { replacements: [appId, offset, limit], type: sequelize.QueryTypes.SELECT})
     .then(function(results) {
-      var data = [];
-      for (var i = 0; i < results.length; i++) {
-        var row = results[i];
-        var bill = _.find(data, function(o) {
-          return row.id == o.id
-        })
-        if (!bill) {
-          data.push({
-            id: row.id,
-            bill_number: row.bill_number,
-            year: row.year,
-            month: row.month,
-            print_data: row.print_data,
-            is_push: row.is_push,
-            username: row.username,
-            active: row.active,
-            created_at: row.created_at,
-            updated_at: row.updated_at,
-            deleted_at: row.deleted_at,
-            unit_id: row.unit_id,
-            property_bill_lines: [{
-              id: row.bill_line_id,
-              work_order_number: row.work_order_number,
-              description: row.description,
-              taxable_amount: row.taxable_amount,
-              tax: row.tax,
-              gross_amount: row.gross_amount,
-              is_pay: row.is_pay,
-              expire_date: row.expire_date
-            }],
-            unit: [{
-              id: row.unit_id,
-              unit_number: row.unit_number,
-              unit_desc: row.unit_desc
-            }]
-          })
-        }
-        else {
-          bill.property_bill_lines.push({
-            id: row.bill_line_id,
-            work_order_number: row.work_order_number,
-            description: row.description,
-            taxable_amount: row.taxable_amount,
-            tax: row.tax,
-            gross_amount: row.gross_amount,
-            is_pay: row.is_pay,
-            expire_date: row.expire_date
-          })
-
-          bill.unit.push({
-            id: row.unit_id,
-            unit_number: row.unit_number,
-            unit_desc: row.unit_desc
-          })
-        }
-      }
 
       var countQuery = 'SELECT count(1) FROM (SELECT DISTINCT id FROM vw_property_bill WHERE app_id = ?';
 
@@ -136,45 +165,35 @@ module.exports = function(app, db, options){
       if (timeOption.length > 0) {
         countQuery += ' AND ' + timeOption
       }
-      if (typeof param.is_pay != 'undefined' &&  param.is_pay != null) {
-        countQuery += ' AND is_pay=' + param.is_pay
+      if (typeof is_pay != 'undefined' &&  is_pay != null) {
+        countQuery += ' AND is_pay=' + is_pay
       }
-
 
       countQuery += ') as count'
 
       sequelize.query(countQuery, { replacements: [appId], type: sequelize.QueryTypes.SELECT})
       .then(function(count) {
 
-        return res.json({
-          success: true,
-          data: data,
-          offset: offset,
-          limit: limit,
-          count: count.length>0?count[0].count:0
-        })
+        return callback(
+          null,
+          results,
+          offset,
+          limit,
+          count.length>0?count[0].count:0
+        )
       })
       .catch(function(err) {
         console.error(err)
-        return res.status(500).json({
-          success: false
-          ,errMsg: err.message
-          ,errors: err
-        })
+        return callback(err)
       })
 
     })
     .catch(function(err) {
       console.error(err)
-      return res.status(500).json({
-        success: false
-        ,errMsg: err.message
-        ,errors: err
-      })
+      return callback(err)
     })
+  }
 
-
-  })
 
   app.use("/billManager", router);
 
